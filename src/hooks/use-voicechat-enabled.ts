d08b476bfbfe9781c7b3c694fa062594a096852e
ninjaninja140 <edenkneale@outlook.com>
2026-08-09 02:06:32 +0100

    Fixes voice chat detection

--- src/hooks/use-voicechat-enabled.ts
+++ src/hooks/use-voicechat-enabled.ts
@@ -1,13 +1,11 @@
-import { useEffect, useState } from "@rbxts/react";
-import { Players, VoiceChatService } from "@rbxts/services";
+import { useEffect, useState } from '@rbxts/react';
+import { Players, VoiceChatService } from '@rbxts/services';
 
 export function useVoicechatEnabled() {
 	const [enabled, setEnabled] = useState(false);
 
 	useEffect(() => {
-		Promise.try(() =>
-			VoiceChatService.IsVoiceEnabledForUserIdAsync(Players.LocalPlayer as unknown as User),
-		).andThen(setEnabled);
+		Promise.try(() => VoiceChatService.IsVoiceEnabledForUserIdAsync(Players.LocalPlayer.User)).andThen(setEnabled);
 	}, []);
 
 	return enabled;
